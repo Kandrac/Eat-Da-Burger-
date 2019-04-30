07ed67dafa74f502abf216751a8d3191467edf76
Brian Kandrac <b_kand@yahoo.com>
2019-04-30 06:13:21 -0400

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
-INSERT INTO burgers (name) VALUES ('Breakfast Burger');
-INSERT INTO burgers (name) VALUES ('Avacado Burger');
-INSERT INTO burgers (name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Gourdon-Hamsey Burger');
+INSERT INTO burgers (name) VALUES ('Poutine on the Ritz Burger');
+INSERT INTO burgers (name) VALUES ('Eggers Cant be cheesers');
+INSERT INTO burgers (name) VALUES ('Fig-eta Bout It Burger');
+INSERT INTO burgers (name) VALUES ('Papaya Was a Rolling Stone Burger');
+INSERT INTO burgers (name) VALUES ('Tarragon in Sixty Seconds Burger');
+INSERT INTO burgers (name) VALUES ('New Bacon-ings');
+INSERT INTO burgers (name) VALUES ('Sweet Chili O’Mine Burger');
+INSERT INTO burgers (name) VALUES (' Olive and Let Die Burger ');
---
+++ public/assets/js/burger.js
@@ -0,0 +1,42 @@
+$(function() {
+    $(".devour-burger").on("click", function(event) {
+      var id = $(this).data("id");
+      var fresh = true;
+  
+      var devouredState = {
+        devoured: fresh
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+      }).then(
+        function() {
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#newBurger").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
